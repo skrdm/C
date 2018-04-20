7bb5f35a9204622b44df659aa144b63035933e95
skrdm <jaqa.jugga@gmail.com>
2018-04-20 23:41:40 +0300

    21 Dynamic

---
+++ 21 Dynamic/Dynamic1.c
@@ -0,0 +1,13 @@
+﻿void push(struct node* head, int data) 
+{
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->data = data;
+	newNode->next = NULL;
+	head = newNode;
+}
+
+void push_test() 
+{
+	List head = build();
+	push(head, 1);
+}
---
+++ 21 Dynamic/Dynamic2.c
@@ -0,0 +1,17 @@
+﻿node* my_copy(node* first) 
+{
+	assert(first != NULL);
+	node i_ptr = first;
+	node cp_ptr = new node(i_ptr->item, i_ptr->next)
+
+		node new_first = cp_ptr;
+
+	while (i_ptr->next != NULL)
+	{
+		node cp_ptr->next = new node(i_ptr->next->item, i_ptr->next->next);
+
+		i_ptr = i_ptr->next;
+		cp_ptr = cp_ptr->next;
+	}
+	return new_first;
+}
